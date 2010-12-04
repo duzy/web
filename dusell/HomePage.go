5816b5f34c67010aeac13f6d57ecf2fc15490f6e
Duzy Chan <code@duzy.info>
2010-12-04 19:15:47 +0800

    make parts of HomePage web.Stringers

--- dusell/HomePage.go
+++ dusell/HomePage.go
@@ -24,21 +24,54 @@ func (h *homePage) MakeFields(app *web.App) (fields interface{}) {
         h.StandardFields["keywords"] = "DuSell Online Store"
         h.StandardFields["description"] = "DuSell Online Store do what you buy"
         h.StandardFields["headline"] = web.NewStringer(headline, app)
-        h.StandardFields["page_content_left"] = "content_left"
-        h.StandardFields["page_content_center"] = "content_center"
-        h.StandardFields["page_content_right"] = "content_right"
-        h.StandardFields["page_content_tail"] = "tail"
+        h.StandardFields["page_content_left"] = web.NewStringer(contentLeft, app)
+        h.StandardFields["page_content_center"] = web.NewStringer(contentCenter, app)
+        h.StandardFields["page_content_right"] = web.NewStringer(contentRight, app)
+        h.StandardFields["page_content_tail"] = web.NewStringer(contentTail, app)
         return
-}
-
-type Headline struct {
-        web.TemplateString
 }
 
 var headline = &Headline {
         "TODO:headline",
 }
 
-func (vm *Headline) MakeFields(app *web.App) interface{} {
-        return interface{}(vm)
+var contentLeft = &ContentLeft {
+        "TODO:content_left",
 }
+
+var contentCenter = &ContentCenter {
+        "TODO:content_center",
+}
+
+var contentRight = &ContentRight {
+        "TODO: content_right",
+}
+
+var contentTail = &ContentTail {
+        "TODO: content_tail",
+}        
+
+type Headline struct {
+        web.TemplateString
+}
+func (vm *Headline) MakeFields(app *web.App) interface{} { return vm }
+
+type ContentLeft struct {
+        web.TemplateString
+}
+func (vm *ContentLeft) MakeFields(app *web.App) interface{} { return vm }
+
+type ContentCenter struct {
+        web.TemplateString
+}
+func (vm *ContentCenter) MakeFields(app *web.App) interface{} { return vm }
+
+type ContentRight struct {
+        web.TemplateString
+}
+func (vm *ContentRight) MakeFields(app *web.App) interface{} { return vm }
+
+type ContentTail struct {
+        web.TemplateString
+}
+func (vm *ContentTail) MakeFields(app *web.App) interface{} { return vm }
