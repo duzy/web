36a1fed20a4a809c2655cd97a242591a3282c505
Duzy Chan <code@duzy.info>
2010-12-24 22:36:30 +0800

    improved findItemsByKeywords JSON test

--- eBay/src/apis_test.go
+++ eBay/src/apis_test.go
@@ -3,7 +3,7 @@ package eBay
 import (
         "testing"
         "strings"
-        "fmt"
+        //"fmt"
 )
 
 func TestAPIFindItemsByKeywords(t *testing.T) {
@@ -14,7 +14,7 @@ func TestAPIFindItemsByKeywords(t *testing.T) {
                 s, err := eb.FindItemsByKeywords("iPhone", 3)
                 if err != nil { t.Error(err); goto finish }
 
-                fmt.Printf("%s\n", s)
+                //fmt.Printf("%s\n", s)
 
                 n := strings.Index(s, "<findItemsByKeywordsResponse ")
                 if n == -1 { t.Error("no tag found: findItemsByKeywordsResponse"); goto finish }
@@ -35,10 +35,29 @@ func TestAPIFindItemsByKeywords(t *testing.T) {
                 eb := NewApp()
                 eb.ResponseFormat = "JSON"
 
-                s, err := eb.FindItemsByKeywords("Nokia N9", 1)
+                s, err := eb.FindItemsByKeywords("Nokia N9", 3)
                 if err != nil { t.Error(err); goto finish }
 
-                fmt.Printf("%s\n", s)
+                //fmt.Printf("%s\n", s)
+
+                n := strings.Index(s, "\"findItemsByKeywordsResponse\":")
+                if n == -1 { t.Error("no prop: \"findItemsByKeywordsResponse\""); goto finish }
+
+                s = s[n+20:len(s)]
+                n = strings.Index(s, "\"item\":")
+                if n == -1 { t.Error("no prop: \"item\""); goto finish }
+
+                s = s[n+7:len(s)]
+                n = strings.Index(s, "[{\"itemId\":")
+                if n == -1 { t.Error("no prop: \"itemId\" [1]"); goto finish }
+
+                s = s[n+10:len(s)]
+                n = strings.Index(s, "{\"itemId\":")
+                if n == -1 { t.Error("no prop: \"itemId\" [2]"); goto finish }
+
+                s = s[n+10:len(s)]
+                n = strings.Index(s, "{\"itemId\":")
+                if n == -1 { t.Error("no prop: \"itemId\" [3]"); goto finish }
         }
 finish:
 }
