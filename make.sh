6b9515b4d8fcf9237d0341a2fa5d44a283b11c2b
Duzy Chan <code@duzy.info>
2011-02-20 14:59:41 +0800

    apply make.sh to the new changes

--- make.sh
+++ make.sh
@@ -5,22 +5,22 @@
 go_tests=`ls src/*_test.go`
 #go_tests="src/db_test.go"
 go_files="
-  src/app.go
-  src/cgi.go
-  src/err.go
-  src/fcgi.go
-  src/viewmgr.go
+  app.go
+  cgi.go
+  err.go
+  fcgi.go
+  viewmgr.go
 "
 
 ( build_pack web && build_testmain web ) || ( echo "rc: web: $?" && exit -1 )
 
 ## test app for FCGIModel
 go_tests=""
-go_files="tst/fcgi_test.go"
+go_files="examples/fcgi_simple/fcgi_test.go"
 
 ( build_exe test.fcgi ) || ( echo "rc: test.fcgi: $?" && exit -1 )
 
-go_files="tst/fcgi_page.go"
+go_files="examples/fcgi_page/fcgi_page.go"
 
 ( build_exe page.fcgi ) || ( echo "rc: page.fcgi: $?" && exit -1 )
 
